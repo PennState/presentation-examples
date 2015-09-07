b092a6460f417312d3f1d583db4deef14bd38abf
Shawn Eion Smith <ses44@psu.edu>
2015-09-07 15:51:18 -0400

    More goofing around

--- presentation-examples-jar/src/main/java/edu/psu/presenations/lambda/OddWords.java
+++ presentation-examples-jar/src/main/java/edu/psu/presenations/lambda/OddWords.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /** 
@@ -19,5 +20,31 @@ public class OddWords
                                              .collect(Collectors.toList());
     
     System.out.println("There are " + oddWords.size() + " odd words");
+    
+    Integer biggest = oddWords.stream()
+                              .map(s -> s.length())
+                              .max(Integer::compare)
+                              .orElse(-1);
+    
+    System.out.println("And the biggest is " + biggest + " characters");
+    
+    Optional<Integer> biggest2 = oddWords.stream()
+                                         .map(s -> s.length())
+                                         .max(Integer::compare);
+    
+    System.out.println("And the biggest2 is " + biggest2.orElse(-1) + " characters");
+    
+    //TODO - What is the the longest word
+    
+    for (int i = 0; i < 1000; ++i)
+    {
+      Optional<String> wordWithExe = Files.lines(Paths.get("/usr/share/dict/words"))
+                                          .parallel()
+                                          .filter(s -> s.contains("exe"))
+                                          .findAny();
+      
+      System.out.println(wordWithExe.get());
+    }
+                                      
   }
 }
