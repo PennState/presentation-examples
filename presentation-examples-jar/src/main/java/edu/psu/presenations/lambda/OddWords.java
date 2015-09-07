423094af859cbf2b0d896cb4018aa41a866be246
Shawn Eion Smith <ses44@psu.edu>
2015-09-07 15:31:05 -0400

    Playing with lambdas

--- presentation-examples-jar/src/main/java/edu/psu/presenations/lambda/OddWords.java
+++ presentation-examples-jar/src/main/java/edu/psu/presenations/lambda/OddWords.java
@@ -1,12 +1,23 @@
 package edu.psu.presenations.lambda;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/** 
+ * Extract all of the odd words from the standard linux words file
+ * @author shawn
+ */
 public class OddWords
 {
-
-  public static void main(String[] args)
+  public static void main(String[] args) throws IOException
   {
+    List<String> oddWords = Files.lines(Paths.get("/usr/share/dict/words"))
+                                             .filter(s -> s.length() % 2 != 0)
+                                             .collect(Collectors.toList());
     
-
+    System.out.println("There are " + oddWords.size() + " odd words");
   }
-
 }
