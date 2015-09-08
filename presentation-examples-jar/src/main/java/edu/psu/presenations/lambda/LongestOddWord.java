4c831717b55d7b19e91fb1a4fb47536e2bfcb21e
Shawn Eion Smith <ses44@psu.edu>
2015-09-08 11:31:12 -0400

    Added old school file reading

--- presentation-examples-jar/src/main/java/edu/psu/presenations/lambda/LongestOddWord.java
+++ presentation-examples-jar/src/main/java/edu/psu/presenations/lambda/LongestOddWord.java
@@ -1,40 +1,47 @@
 package edu.psu.presenations.lambda;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
-/** 
+/**
  * Extract all of the odd words from the standard linux words file
+ * 
  * @author shawn
  */
 public class LongestOddWord
 {
   public static void main(String[] args) throws IOException
   {
-    List<String> oddWords = Files.lines(Paths.get("/usr/share/dict/words"))
-                                             .filter(s -> s.length() % 2 != 0)
-                                             .collect(Collectors.toList());
+    FileInputStream fis = new FileInputStream(new File("/usr/share/dict/words"));
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+    String currentLine = null;
     int length = -1;
     String word = null;
-    
-    for (String s : oddWords)
+
+    while ((currentLine = br.readLine()) != null)
     {
-      if (s.length() > length)
+      if (currentLine.length() > length)
       {
-        length = s.length();
-        word = s;
+        length = currentLine.length();
+        word = currentLine;
       }
     }
-    
+
+    br.close();
+
     System.out.println("Longest word = " + word + " at " + length + " characters");
-    
+
     String longest = Files.lines(Paths.get("/usr/share/dict/words"))
-                                      .filter(s -> s.length() % 2 != 0)
-                                      .reduce("", (a, b) -> (b.length() > a.length()) ? b : a);
-    
+                          .filter(s -> s.length() % 2 != 0)
+                          .reduce("", (a, b) -> (b.length() > a.length()) ? b : a);
+
     System.out.println("Longest word = " + longest + " at " + longest.length() + " characters");
 
   }
